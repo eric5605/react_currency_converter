42e505802a3524e770bc5a0e18253fb700fa5af7
eric5605 <eah7279@live.com>
2020-11-18 15:46:54 -0500

    fixed navbar and links

--- src/Header.js
+++ src/Header.js
@@ -7,13 +7,9 @@ import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Header = () => {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-light bg-light">
-         <Link to="/chart/"><span className="navbar-brand h1">Chart</span></Link>
-
-         <Link to="/"><span className="navbar-brand  h1">Home</span></Link>
-
-         <Link to="/currencies/"><span className="navbar-brand  h1">Currencies Table</span></Link>
-     </nav>
+      <nav className="navbar justify-content-center">
+        <Link to="/"><span className="navbar-brand mb-0 headline">Currency Exchange Rates</span></Link>
+      </nav>
     </React.Fragment>
   )
 }
